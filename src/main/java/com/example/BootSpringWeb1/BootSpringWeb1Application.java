167f38c969937dea4823b6f78c2db11f8e378d5a
loredanat219 <loredanat219@gmail.com>
2022-07-05 17:27:38 +0300

    spring

---
+++ src/main/java/com/example/BootSpringWeb1/BootSpringWeb1Application.java
@@ -0,0 +1,13 @@
+package com.example.BootSpringWeb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringWeb1Application.class, args);
+	}
+
+}
